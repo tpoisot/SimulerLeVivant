43060a077897f9afac01c0e35fa3c01858086114
tpoisot <timothee.poisot@umontreal.ca>
2026-01-29 09:33:14 -0500

    up

--- seances/03_genetics.jl
+++ seances/03_genetics.jl
@@ -1,8 +1,86 @@
+# ---
+# title: Séance 3
+# topic: Génétique des populations
+# author: Timothée Poisot
+# ---
+
+# # Concepts principaux
+
+# ## Fonctions anonymes
+
+# Dans la séance précédente, nous avons commencé a écrire des fonctions. Dans
+# certaines situations, il est nécessaire d'utiliser des fonctions, mais
+# puisqu'elles sont très simples et à usage unique, on dispose d'une syntaxe
+# simplifiée, `argument -> operation`:
+
+x -> 2*x + 1
+
+# Le symbole `->` est simplement `-` puis `>`.
+
+# Ces fonctions peuvent s'utiliser comme des fonctions régulières:
+
+(x -> 2*x + 1)(3)
+
+# ## Mapping et slicing
+
+# On peut automatiser certaines opérations _via_ la fonction `map`:
+
+import Random
+Random.seed!(2045)
+x = rand(1:5, 10)
+
+#-
+
+map(v -> sqrt(v + 1), x)
+
+# Cette syntaxe est équivalente à l'utilisation d'une boucle `for`: on applique
+# la fonction anonyme `x -> sqrt(x + 1)` à chaque élément du vecteur.
+
+# On peut aussi appliquer la fonction `map` sur certaines dimensions d'un objet,
+# _via_ `mapslices`. Par exemple:
+
+V = rand(1:10, 3, 4)
+
+#-
+
+mapslices(x -> minimum(x) % 2 == 0, V, dims=1)
+
+# Cette fonction va appliquer la fonction `minimum(x) % 2 == 0`, qui renvoie
+# `true` si la plus petite valeur de la colonne est un nombre pair, et `false`
+# sinon, pour chaque _colonne_ de la matrice.
+
+# On peut faire la même chose pour les lignes:
+
+mapslices(x -> minimum(x) % 2 == 0, V, dims=2)
+
+# Deux choses sont importantes ici:
+
+# D'abord, la dimension 1 correspond aux _colonnes_, et pas aux _lignes_. C'est
+# parce que Julia stocke les colonnes en premier dans la mémoire, et non les
+# lignes.
+
+# Enfin, l'objet est retourné sous forme de _matrice_, parce que l'objet donné
+# en agument est une matrice.
+
+# On peut éliminer les dimensions qui ne sont plus nécessaires pour obtenir un
+# vecteur:
+
+v = mapslices(x -> minimum(x) % 2 == 0, V, dims=2)
+dropdims(v, dims=2)
+
+# Notez qu'on peut utiliser `map` et `mapslices` avec le nom d'une fonction qui
+# accepte un unique argument:
+
+mapslices(sum, V, dims=1)
+
+# ## Filtres
+
+# # Simulation: maintien du polymorphisme
+
 using CairoMakie
 CairoMakie.activate!(px_per_unit=2.0)
 
 import StatsBase
-import Random
 
 cells = 100
 generations = 501
@@ -30,7 +108,7 @@ for generation in 2:generations
 end
 
 # Fonction pour les couleurs
-colormap = dropdims(mapslices(x -> CairoMakie.Colors.RGB(x...), lattice, dims=3), dims=3)
+colormap = dropdims(mapslices(x -> CairoMakie.Colors.RGB(x...), lattice, dims=3), dims=3);
 
 # Heatmap et diversité
 f = Figure(; size=(700, 500))
@@ -43,3 +121,13 @@ ylims!(plax, 1, 8)
 xlims!(plax, 0, generations-1)
 xlims!(ax, 0, generations-1)
 f
+
+# # Suggestions pour le premier devoir
+
+# ## Paysage circulaire
+
+# ## Sélection génétique
+
+# ## Déséquilibre de liaison
+
+# ## Traits quantitatifs
