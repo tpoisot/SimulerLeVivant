4962504c412a8ea18895f3974a9c9b46f064e929
tpoisot <timothee.poisot@umontreal.ca>
2026-01-29 12:21:47 -0500

    template

---
+++ devoirs/modele_devoirs.jl
@@ -0,0 +1,53 @@
+# ---
+# cours: BIO 2045
+# devoir: Devoir 1
+# titre: 
+# date: YYYY-MM-DD
+# author:
+#   nom: 
+#   prenom: 
+#   matricule: 
+#   courriel: 
+# ---
+
+# # Introduction
+
+# Le texte de votre introduction va ici.
+
+# Vous pouvez utiliser la syntaxe suivante pour l'_italique_, le **gras**, et
+# les caractères `monospace`.
+
+# L'introduction doit avoir les sections suivantes:
+
+# ## Mise en contexte
+
+# Décrivez de manière sommaire la situation biologique
+
+# ## Question
+
+# Introduisez la question que vous allez aborder avec le modèle
+
+# ## Hypothèse et résultats attendus
+
+# Clarifiez vos attentes par rapport au résultat de la simulation
+
+# # Description du modèle
+
+# En utilisant autant de sous-sections que nécessaire, expliquez le modèle, ses
+# suppositions, et les décisions principales
+
+# On commence une sous-section avec # ## Titre
+
+# # Code pour le modèle
+
+# En utilisant autant de sous-sections que nécessaire, expliquez le code que
+# vous utilisez pour simuler le modèle. Le texte est aussi important que le code
+# en lui-même, et doit faire des liens entre les choix de programmation et la
+# question biologique.
+
+# # Résultat des simulations et discussion
+
+# Présentez les résultats des simulations, en faisant un lien avec la question
+# initiale.
+
+# Concluez sur le résultat, et sur les limitations du modèle.
