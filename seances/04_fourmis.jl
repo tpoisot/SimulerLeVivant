0cb91e7f8927e1443368e730e195698fbb81856f
tpoisot <timothee.poisot@umontreal.ca>
2026-02-05 11:47:24 -0500

    partie concepts

--- seances/04_fourmis.jl
+++ seances/04_fourmis.jl
@@ -8,18 +8,148 @@
 
 # ## Commentaires
 
+# Dans les séances précédentes, nous avons vu qu'utiliser des noms de variables
+# explicites permet de rendre le code plus lisible, et donc plus facile a
+# modifier.
+
+# Dans certains cas, il est important d'expliciter certaines parties du code en
+# utilisant des commentaires. Ces commentaires servent une fonction différente:
+# ils permettent de clarifier _ce que le code doit faire_.
+
+# Par exemple, le code suivant n'est pas une bonne utilisation des commentaires:
+
+import Random
+Random.seed!(2045)
+
+#-
+
+## On génère trois valeurs aléatoires
+a = rand(3)
+## Et on les multiplie par 2π et ensuite on enlève π
+b = map(x -> (x * 2π) - π, a)
+
+# Le même code avec des commentaires utiles serait
+
+## On veut générer des points qui soient disposés de manière aléatoire sur le périmètre d'un cercle.
+
+## On commence par gnérer des valeurs aléatoires de manière uniforme entre 0 et 1
+a = rand(3)
+## On projette ensuite ces valeurs entre -π et π pour que ces valeurs soient uniformément réparties mais deviennent des angles
+b = map(x -> (x * 2π) - π, a)
+
+# Notez que le code est le même, mais que la compréhension du code est facilité
+# dans la deuxième version: le code lui-même indique ce qu'on fait, et les
+# commentaires indiquent _pourquoi_ on le fait.
+
+# **NB**: dans les prochains devoirs, nous utiliserons le package
+# [Literate](https://fredrikekre.github.io/Literate.jl/v2/) pour transformer du
+# code commenté en documents. Dans ce contexte la, les commentaires qui devront
+# _rester_ dans le code seront notés par `##`.
+
 # ## Documentation
 
-# ## Passage par référence
+# Les fonctions que nous allons écrire peuvent (doivent!) avoir leur propre
+# documentation. La documentation d'une fonction se compose au minimum de trois
+# choses:
 
-# ## Fonctions qui modifient leurs arguments
+# 1. le _prototype_ de la fonction
+# 1. une explication de ce que la fonction permet de faire
+# 1. une explication des arguments de la fonction
+
+# Toutes ces informations forment le _docstring_ de la fonction, et s'organisent
+# de la manière suivante:
+
+"""
+    moyenne(x)
+
+Cette fonction calcule la moyenne arithmétique d'un vecteur de nombres `x` donné
+en argument. 
+
+`x` doit être un vecteur de nombres.
+"""
+function moyenne(x)
+    somme = sum(x)
+    n = length(x)
+    return somme / n
+end
+
+# Cette documentation est ensuite accessible _via_ `?moyenne`. Plus
+# d'information sur les _docstring_ est disponible dans le manuel de Julia, dont
+# le lien est disponible dans le plan de cours.
+
+# ## Passage par référence et modification des arguments
+
+# À l'inverse de langages de programmation comme R, Julia évite de dupliquer des
+# objets quand les fonctions ont besoin de les utiliser. Cela rend le code plus
+# efficace, mais crée des complications possibles.
+
+# Prennons la matrice suivante:
+
+V = rand(1:5, 3, 6)
+
+# Et la fonction suivante:
+
+function modif(X)
+    X[1,1] = rand(30:50)
+    return X
+end
+
+# Si on applique cette fonction à `V`, on obtient
+
+modif(V)
+
+# Si on affiche maintenant la matrice `V`, on obtient
+
+V
+
+# Regardez bien la première position de cette matrice: `V` a été _modifié_ par
+# la fonction `modif`. C'est parce que cette fonction n'a pas reçu une copie de
+# `V`, mais bien la matrice elle-même.
+
+# Pour indiquer qu'une fonction modifie son premier argument, on utilise la
+# notation `!`:
+
+function modif!(X)
+    X[1,1] = rand(50:70)
+    return X
+end
+
+# Cette notation est beaucoup plus explicite, et indique qu'un ou plusieurs
+# arguments vont être modifiés par la fonction (et on sait écrire la
+# documentation pour indiquer lesquels, et comment!).
+
+# Si on souhaite ne pas modifier la matrice `V`, il existe un _design pattern_
+# très commun en Julia:
+
+function modif(X)
+    Y = copy(X)
+    modif!(Y)
+    return Y
+end
+
+# Cette nouvelle fonction va d'abord _copier_ `X`, puis modifier cette copie à
+# l'aide de la fonction qui modifie son argument, et retourner la copie
+# modifiée.
+
+# On parle en général de `modif` comme _non-mutating_, et de `modif!` comme
+# _mutating_.
+
+# ## Application d'une fonction sur une collection
+
+# La semaine dernière, nous avons introduit `map` pour appliquer une fonction
+# sur une collection. Il existe une syntaxe plus simple pour la vectorisation
+# (qui est décrite dans le manuel dans la section sur le _broadcasting_):
+
+(x -> 2*x).(rand(3))
+
+# La notation `fonction.(argument)` va appliquer `fonction` à chaque élément de
+# la collection `argument`. Nous allons utiliser cette notation très souvent
+# dans les séances suivantes.
 
 # # Modèle
 
-
 using CairoMakie
 using Statistics
-using ProgressMeter
 using StatsBase
 CairoMakie.activate!(px_per_unit = 6.0)
 
@@ -30,7 +160,6 @@ function disposition_points(n)
     radii = sqrt.(rand(length(angles)) .* 3.0 .+ 1.0)
     x = cos.(angles) .* radii
     y = sin.(angles) .* radii
-
     stops = permutedims(hcat(x, y))
     return stops
 end
