75fed9db2ce6d1e4d4ba770eb8cda99296086eca
tpoisot <timothee.poisot@umontreal.ca>
2026-02-05 15:17:04 -0500

    mix

--- seances/04_fourmis.jl
+++ seances/04_fourmis.jl
@@ -90,7 +90,7 @@ V = rand(1:5, 3, 6)
 # Et la fonction suivante:
 
 function modif(X)
-    X[1,1] = rand(30:50)
+    X[1, 1] = rand(30:50)
     return X
 end
 
@@ -110,7 +110,7 @@ V
 # notation `!`:
 
 function modif!(X)
-    X[1,1] = rand(50:70)
+    X[1, 1] = rand(50:70)
     return X
 end
 
@@ -140,153 +140,337 @@ end
 # sur une collection. Il existe une syntaxe plus simple pour la vectorisation
 # (qui est décrite dans le manuel dans la section sur le _broadcasting_):
 
-(x -> 2*x).(rand(3))
+(x -> 2 * x).(rand(3))
 
 # La notation `fonction.(argument)` va appliquer `fonction` à chaque élément de
 # la collection `argument`. Nous allons utiliser cette notation très souvent
 # dans les séances suivantes.
 
-# # Modèle
+# # Simulation
+
+# Nous allons créer un modèle qui utilise la métaphore des colonies de fourmis
+# pour résoudre un problème d'optimisation.
 
 using CairoMakie
 using Statistics
 using StatsBase
-CairoMakie.activate!(px_per_unit = 6.0)
+CairoMakie.activate!(px_per_unit=6.0)
 
-# ## LOL
+# Nous allons tenter de résoudre le problème suivant: si on dispose d'une liste
+# de points, dans l'espace, quel est le trajet qui permet de visiter l'ensemble
+# de ces points et de revenir au départ, en faisant le trajet le plus court
+# possible?
+
+# Pour commencer, nous allons générer des points qui sont disposés uniformément
+# sur un cercle, avec une distance au centre entre 5 et 10 unités.
 
 function disposition_points(n)
     angles = rand(n) .* 2π
-    radii = sqrt.(rand(length(angles)) .* 3.0 .+ 1.0)
+    radii = sqrt.(rand(length(angles)) .* 5 .+ 5)
     x = cos.(angles) .* radii
     y = sin.(angles) .* radii
     stops = permutedims(hcat(x, y))
     return stops
 end
 
-points = 100
+#-
 
-P = zeros(Float64, points, points)
-D = zeros(Float64, points, points)
+points = 20
+xy = disposition_points(points);
 
-xy = disposition_points(points)
+#-
 
-for i in 1:points
-    for j in 1:points
-        D[i,j] = sqrt(sum((xy[:,i] .- xy[:,j]) .^2.0))
+f = Figure()
+ax = Axis(f[1, 1], aspect=DataAspect())
+scatter!(ax, xy, color=:white, strokecolor=:black, strokewidth=1.5, markersize=12)
+hidespines!(ax)
+hidedecorations!(ax)
+f
+
+# L'objectif est donc de trouver un chemin qui parte d'un point, visite tous les
+# points, et retourne au point de départ. Le _challenge_ est de trouver le
+# chemin qui accumule la plus petite distance totale.
+
+# ## Mesure de la distance entre les points
+
+# La distance entre les points est constante, et nous allons donc la calculer
+# une seule fois. Puisque nous avons un nombre de points $n$, il faut préparer
+# une matrice de taille $n \times n$.
+
+D = zeros(Float64, points, points);
+
+# Nous allons donc stocker la distance entre les points dans cette matrice:
+
+function distance_entre_points!(D, xy)
+    for i in axes(xy, 2)
+        for j in axes(xy, 2)
+            D[i, j] = sqrt(sum((xy[:, i] .- xy[:, j]) .^ 2.0))
+        end
     end
+    return D
 end
 
-scatter(xy)
+# **NB**: vous devrez regarder la documentation de `axes`.
 
-function walk_on_graph(D, P, i)
-    α = 0.9
-    β = 1.5
-    n_sites = size(D, 1)
+# On va ensuite appliquer cette fonction.
 
-    visites = zeros(Bool, n_sites)
-    visites[i] = true
+distance_entre_points!(D, xy);
 
-    chemin = [i]
+# Notez qu'on n'a pas besoin de stocker le résultat de cette opération: la
+# matrice `D` est modifiée directement!
 
-    while sum(visites) != n_sites
-        voisins = Int64[]
-        cible = Float64[]
-        for voisin in setdiff(1:n_sites, chemin)
-            pheromones = max(P[last(chemin), voisin], 1e-5)
-            poids = (pheromones^α)/D[last(chemin), voisin]^β
-            push!(cible, poids)
-            push!(voisins, voisin)
-        end
-        
-        next_site = sample(voisins, Weights(cible))
-        push!(chemin, next_site)
-        visites[next_site] = true
+# ## Représenter un chemin
+
+# Un chemin est une séquence de points, tous uniques, qui représente un circuit
+# complet dans la liste des points. On prend le premier point au hasard, puis on
+# choisit ensuite chaque point parmis la liste de ceux qui n'ont pas encore été
+# visités:
+
+function chemin_aleatoire(xy)
+    pool = collect(axes(xy, 2))
+    chemin = []
+    for i in axes(xy, 2)
+        push!(chemin, popat!(pool, rand(eachindex(pool))))
     end
+    push!(chemin, first(chemin))
     return chemin
 end
 
-"""
-    chemin_distance(chemin, D)
+# **NB**: vous devrez regarder la documentation de `collect` et de `pop!` / `popat!`
 
-Distance totale du chemin, incluant le retour au point initial
+C = chemin_aleatoire(xy);
 
-- `chemin`: vecteur de positions qui indique l'ordre de visite des sites 
-- `D`: matrice de distance entre les sites
-"""
-function chemin_distance(chemin, D)
-    # d représente la distance pour revenir au début du cycle
-    #
-    # on veut créer un circuit, et le chemin s'arrête au dernier
-    # site visité
-    # 
-    # le chemin indique l'ordre de visite des sites
-    #
-    # D est la matrice de distance entre tous les points
-    d = D[chemin[end], chemin[begin]]
-    
-    # on va calculer la distance totale du reste de chemin
-    # on a deja la distance du retour vers le premier site
-    for i in 2:length(chemin)
-        # on lit dans D la distance entre
-        # le site visité à la position i
-        # et le site visité juste avant (position i-1)
-        #
-        # on ajouter cette distance à d
-        d += D[chemin[i-1], chemin[i]]
+# On peut maintenant visualiser ce chemin
+
+f = Figure()
+ax = Axis(f[1, 1], aspect=DataAspect())
+for i in 2:length(C)
+    linesegments!(ax, xy[1, [C[i-1], C[i]]], xy[2, [C[i-1], C[i]]], color=:grey50)
+end
+scatter!(ax, xy, color=:white, strokecolor=:black, strokewidth=1.5, markersize=12)
+hidespines!(ax)
+hidedecorations!(ax)
+f
+
+# Ce chemin n'est pas optimal -- il est assez long. Quelle est sa longueur?
+
+function distance_chemin(C, D)
+    d = 0.0
+    if last(C) != first(C)
+        push!(C, first(C))
     end
-    
-    # d contient la distance totale du cycle
-    # distance du chemin + distance du retour au premier point
-    # on renvoie d
+    for i in 2:length(C)
+        d += D[C[i-1], C[i]]
+    end
     return d
 end
 
-function pheromones!(P, chemin, D)
-    Q = size(P, 1)
-    score = Q / chemin_distance(chemin, D)
-    P[chemin[end], chemin[1]] += score
+# La distance de notre chemin est la suivante:
+
+distance_chemin(C, D)
+
+# ## Améliorer la distance du chemin
+
+# Pour améliorer la distance du chemin, nous allons laisser une colonie de
+# fourmis virtuelles se déplacer entre les points. Les fourmis se déplacent en
+# prennant en compte deux informations.
+
+# D'abord, une fourmi a plus de chances d'aller visiter un site qui est proche
+# du site sur lequel elle se trouve -- c'est une optimisation à l'échelle
+# "micro", qui favorise les chemins localements courts.
+
+# Ensuite, les fourmis vont favoriser les chemins qui ont déjà été visités par
+# d'autres fourmis. Pour rendre ce mécanisme possible, les fourmis vont ajouter
+# des phéromones à chaque fois qu'elles se déplacent entre deux points.
+
+# Il faut donc stocker ces phéromones dans une matrice. Elle représente une
+# autre vision de la distance entre les deux sites, et on peut donc la générer
+# en utilisant les informations sur la taille de la matrice de distance:
+
+P = zeros(Float64, size(D)...);
+
+# Le processus de décision d'une fourmi est donc résumé de la manière suivante:
+# la probabilité qu'un site soit choisi pour le prochain mouvement est
+# proportionnel à $P^\alpha$, et inversement proportionnel à $D^\beta$, avec
+# $\alpha$ et $\beta$ qui sont des paramètres que l'on peut varier pour rendre
+# la distance ou les phéromones plus importants.
+
+# ## Exemple avec une seule fourmi
+
+# Imaginons que la fourmi soit initialement sur le site 1, on commence par
+# identifier les sites qui sont disponibles pour une visite:
+
+sites_disponibles = filter(x -> x != 1, 1:points);
+
+# On choisit les valeurs pour $\alpha$ et $\beta$:
+
+α, β = 1.0, 1.0
+
+# Et on mesure les distances avec tous ces sites:
+
+D[1, sites_disponibles] .^ β;
+
+# On peut choisir un de ces sites au hasard en prennant en compte les poids:
+
+next_site = StatsBase.sample(sites_disponibles, Weights(1.0 ./ (D[1, sites_disponibles] .^ β)))
+
+# Et visualiser le site suivant:
+
+f = Figure()
+ax = Axis(f[1, 1], aspect=DataAspect())
+linesegments!(ax, xy[1, [1, next_site]], xy[2, [1, next_site]], color=:grey50)
+scatter!(ax, xy, color=:white, strokecolor=:black, strokewidth=1.5, markersize=12)
+hidespines!(ax)
+hidedecorations!(ax)
+f
+
+# Plus la valeur de $\beta$ est élevé, plus la probabilité que les sites
+# distants soient choisis diminue.
+
+# Pour le moment, on ne prend pas en compte la valeur des phéromones, parce
+# qu'il faut identifer un mécanisme pour que les fourmis déposent ces
+# phéromones. Avant de faire ce travail, on peut regarder quel chemin serait
+# choisit uniquement sur la base de la distance
+
+function chemin_proximity(xy, D)
+    pool = collect(axes(xy, 2))
+    chemin = []
+    push!(chemin, popat!(pool, rand(eachindex(pool))))
+    while !isempty(pool)
+        next_site_index = StatsBase.sample(eachindex(pool), Weights(1.0 ./ (D[last(chemin), pool])))
+        push!(chemin, popat!(pool, next_site_index))
+    end
+    push!(chemin, first(chemin))
+    return chemin
+end
+
+# new test
+
+C = chemin_proximity(xy, D .^ β);
+
+# On peut maintenant visualiser ce chemin
+
+f = Figure()
+ax = Axis(f[1, 1], aspect=DataAspect())
+for i in 2:length(C)
+    linesegments!(ax, xy[1, [C[i-1], C[i]]], xy[2, [C[i-1], C[i]]], color=:grey50)
+end
+scatter!(ax, xy, color=:white, strokecolor=:black, strokewidth=1.5, markersize=12)
+hidespines!(ax)
+hidedecorations!(ax)
+f
+
+# La distance de ce chemin est meilleure que la version aléatoire:
+
+distance_chemin(C, D)
+
+# On va maintenant déposer les phéromones. Dans cette simulation, on considère
+# que les phéromones sont directionnels. La logique pour les phéromones est la
+# suivante. Chaque fourmi dispose d'une quantité total de phéromone, et leur
+# trace est donc moins intense pour un chemin plus long. Pour simplifier, on va
+# considérer que chaque fourmi dispose d'une unité de phéromone pour chaque
+# site.
+
+function pheromones!(P, chemin, D, n)
+    qte_pheromone = size(P, 1)
+    score = qte_pheromone / distance_chemin(chemin, D)
     for i in 2:length(chemin)
-        P[chemin[i-1], chemin[i]] += score
+        #P[sort([chemin[i-1], chemin[i]])...] += score*n
+        P[chemin[i-1], chemin[i]] += score*n
     end
     return P
 end
 
-track = zeros(Float64, 120)
-n_fourmis = 50
-evaporation_rate = 0.99
+# On applique cette fonction pour mettre à jour les phéromones qui sont
+# maintenant sur la surface couverte par les points:
 
-@showprogress for i in 1:length(track)
+pheromones!(P, C, D, 1);
 
-    chemins = [walk_on_graph(D, P, rand(1:points)) for _ in 1:n_fourmis]
+# ## Choix du chemin optimal
 
-    # Remove the chemins with more than the median chemin_distance
-    distances = [chemin_distance(chemin, D) for chemin in chemins]
-    median_distance = median(distances)
-    chemins = chemins[findall(distances .<= median_distance)]
+# Nous allons maintenant mettre à jour la fonction complète:
 
-    for chemin in chemins
-        pheromones!(P, chemin, D)
+function chemin_complet(xy, D, P)
+    pool = collect(axes(xy, 2))
+    chemin = []
+    push!(chemin, popat!(pool, rand(eachindex(pool))))
+    while !isempty(pool)
+        next_site_index = StatsBase.sample(eachindex(pool), Weights((P[last(chemin), pool] .+ 1e-2) ./ (D[last(chemin), pool])))
+        push!(chemin, popat!(pool, next_site_index))
     end
-    P ./= length(chemins)
-    #P .+= rand(size(P)).*0.02 .- 0.01
-    P .*= evaporation_rate
-
-    track[i] = minimum(distances)
-
+    push!(chemin, first(chemin))
+    return chemin
 end
 
-# Plotting the points and lines colored by the value of P
-fig = Figure(size=(600, 680))
-gl = fig[1,1] = GridLayout()
-ax = Axis(gl[1, 1], aspect=1)
-ax2 = Axis(gl[2,1], yscale=sqrt)
-lines!(ax2, track, color=:purple, linewidth=2)
-hidedecorations!(ax2)
+# Le principe de cette simulation est de faire se déplacer _beaucoup_ de fourmis
+# en même temps:
 
-scatter!(ax, xy[1, :], xy[2, :], color=:black)
+n_fourmis = 50
+
+# On peut maintenant générer un chemin pour chaque fourmi:
+
+chemins = []
+for i in 1:n_fourmis
+    push!(chemins, chemin_complet(xy, D .^ β, P .^ α))
+end
+
+# Après que toutes les fourmis ont terminé leur parcours, on va maintenant
+# mettre à jour les phéromones:
+
+for i in 1:n_fourmis
+    pheromones!(P, chemins[i], D, 1/n_fourmis)
+end
+
+# Voici le résultat sous forme de figure.
+
+f = Figure()
+ax = Axis(f[1, 1], aspect=DataAspect())
+for i in axes(P, 1)
+    for j in axes(P, 2)
+        lines!(ax, [xy[1, i], xy[1, j]], [xy[2, i], xy[2, j]], color=P[i, j], colorrange=extrema(P))
+    end
+end
+scatter!(ax, xy, color=:white, strokecolor=:black, strokewidth=1.5, markersize=12)
 hidespines!(ax)
 hidedecorations!(ax)
+f
 
-rowsize!(gl, 2, Relative(0.2))
-fig
+# ## Évaporation
+
+# Pour permettre d'optimiser ce chemin, on veut enlever un peu de phéromones à
+# chaque génération. Cette opération est assez simple:
+
+evaporation_rate = 0.9
+P .*= evaporation_rate;
+
+# Une fois que cette évaporation est appliquée, il suffit de relancer une
+# nouvelle génération de fourmis:
+
+# ## Simulation
+
+P = zeros(Float64, size(D)...)
+for generation in 1:20
+    global chemins = []
+    for i in 1:n_fourmis
+        push!(chemins, chemin_complet(xy, D .^ β, P .^ α))
+    end
+    
+    for i in 1:n_fourmis
+        pheromones!(P, chemins[i], D, 1/n_fourmis)
+    end
+    P .*= evaporation_rate
+end
+
+# Voici le résultat sous forme de figure.
+
+f = Figure()
+ax = Axis(f[1, 1], aspect=DataAspect())
+for i in axes(P, 1)
+    for j in axes(P, 2)
+        lines!(ax, [xy[1, i], xy[1, j]], [xy[2, i], xy[2, j]], color=P[i, j], alpha=P[i, j], colorrange=extrema(P), colormap=:Greys)
+    end
+end
+scatter!(ax, xy, color=:white, strokecolor=:black, strokewidth=1.5, markersize=12)
+hidespines!(ax)
+hidedecorations!(ax)
+f
