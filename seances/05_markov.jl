29fff3bda23a39d614542112aa397dbceae1d10b
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-02-23 17:54:27 -0500

    markov code

---
+++ seances/05_markov.jl
@@ -0,0 +1,88 @@
+using CairoMakie
+using Distributions
+
+import Random
+Random.seed!(2045)
+
+function check_transition_matrix!(T)
+    for ligne in axes(T, 1)
+        if sum(T[ligne, :]) != 1
+            @warn "La somme de la ligne $(ligne) n'est pas égale à 1 et a été modifiée"
+            T[ligne, :] ./= sum(T[ligne, :])
+        end
+    end
+    return T
+end
+
+function check_function_arguments(transitions, states)
+    if size(transitions, 1) != size(transitions, 2)
+        throw("La matrice de transition n'est pas carrée")
+    end
+
+    if size(transitions, 1) != length(states)
+        throw("Le nombre d'états ne correspond psa à la matrice de transition")
+    end
+    return nothing
+end
+
+function simulation(transitions, states; generations=500, stochastic=false)
+    
+    check_transition_matrix!(transitions)
+    check_function_arguments(transitions, states)
+
+    _data_type = stochastic ? Int64 : Float32
+    timeseries = zeros(_data_type, length(states), generations + 1)
+    timeseries[:, 1] = states
+
+    for generation in Base.OneTo(generations)
+        if stochastic
+            for state in axes(timeseries, 1)
+                pop_change = rand(Multinomial(timeseries[state, generation], transitions[state, :]))
+                timeseries[:, generation+1] .+= pop_change
+            end
+        else
+            pop_change = (timeseries[:, generation]' * transitions)'
+            timeseries[:, generation+1] .= pop_change
+        end
+    end
+
+    return timeseries
+end
+
+# States
+# Barren, Grass, Shrubs
+s = [0, 500, 0]
+states = length(s)
+patches = sum(s)
+
+# Transitions
+T = zeros(Float64, states, states)
+T[1, :] = [110, 8, 0]
+T[2, :] = [2, 120, 3]
+T[3, :] = [1, 0, 94]
+
+states_names = ["Barren", "Grasses", "Shrubs"]
+states_colors = [:grey40, :orange, :teal]
+
+# Simulations
+
+f = Figure()
+ax = Axis(f[1, 1], xlabel="Nb. générations", ylabel="Nb. parcelles")
+
+# Stochastic simulation
+for _ in 1:10
+    sto_sim = simulation(Tm, s; stochastic=true, generations=200)
+    for i in eachindex(s)
+        lines!(ax, sto_sim[i, :], color=states_colors[i], alpha=0.2)
+    end
+end
+
+# Deterministic simulation
+det_sim = simulation(Tm, s; stochastic=false, generations=200)
+for i in eachindex(s)
+    lines!(ax, det_sim[i, :], color=states_colors[i], alpha=1, label=states_names[i])
+end
+
+axislegend(ax)
+tightlimits!(ax)
+current_figure()
